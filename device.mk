df49c090054fcb382938ed132138f291ed3aa4b2
TTTT555 <albukerko@gmail.com>
2020-04-06 22:29:53 +0200

    45

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := framework-res
+#PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 
 # Include product configs
 include $(LOCAL_PATH)/nx511j.mk
